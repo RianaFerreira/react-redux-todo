1cd49e3ebf4edf2a223fbbf1962f76599df730f9
Riana Ferreira <rianaferreira@gmail.com>
2016-12-07 12:28:26 +1000

    Setup development and global dependencies with NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 var envFile = require('node-env-file');
 
+// $ NODE_ENV=production webpack -p
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 // pass the environment variables from test.env and development.env into webpack
@@ -24,6 +25,11 @@ module.exports = {
       '$': 'jquery',
       'jQuery': 'jquery'
     }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    }),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
@@ -31,7 +37,7 @@ module.exports = {
         AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
         DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
         STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET),
-        MESSAGING_SENDER_ID: JSON.stringify(process.env.MESSAGING_SENDER_ID),
+        MESSAGING_SENDER_ID: JSON.stringify(process.env.MESSAGING_SENDER_ID)
       }
     })
   ],
@@ -66,5 +72,5 @@ module.exports = {
     ]
   },
   sassLoader: { includePaths: [path.resolve(__dirname, './node_modules/foundation-sites/scss')] },
-  devtool: 'cheap-module-eval-source-map'
+  devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
 }
